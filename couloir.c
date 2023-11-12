4cd87f20caee14a9d84eaa996401a6070ff5ca3a
Tardisdoc12 <jean.anquetil@livecampus.tech>
2023-11-12 21:22:50 +0100

    ajout des fonctions

--- couloir.c
+++ couloir.c
@@ -1,8 +1,15 @@
+// C File standard
+//--------------------------------------------------------------------------------
+//  File couloir.c
+// Date: 12 Nov 2023
+//--------------------------------------------------------------------------------
+
 #include "couloir.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//--------------------------------------------------------------------------------
 
 void init(couloir *c, int largeur, const char *sequence){
 	c->largeur = largeur;
@@ -10,9 +17,33 @@ void init(couloir *c, int largeur, const char *sequence){
 	strcpy(c->sequence,sequence);
 }
 
+//--------------------------------------------------------------------------------
+
 void affiche(couloir *c){
 	for (int i=0; (c->sequence)[i] != '\0'; i++) {
-	printf("%c\n",(c->sequence)[i]);
+		printf("%c\n", (c->sequence)[i]);
+		int count = count_occurence_direction(c, i, (c->sequence)[i]);
+		if ((c->sequence)[i] == 'N'){
+			continue;
+		}
+		else if ((c->sequence)[i]=='S'){
+			if (count > 1){
+				printf("# #\n");
+			}
+			else{
+				printf("#\n");
+			}
+			continue;
+		}
+		else if ((c->sequence)[i]=='E'){
+			continue;
+		}
+		else if ((c->sequence)[i]=='W'){
+			continue;
+		}
+		else {
+			printf("erreur de saisie\n");
+		}
 	}
 	//printf("%d\t%d",count_north(c->sequence),count_west(c->sequence));
 	//printf("\n");
@@ -31,10 +62,31 @@ void affiche(couloir *c){
 	}*/
 }
 
+//--------------------------------------------------------------------------------
+
+int count_occurence_direction(couloir *c, int index, char direction){
+	int count_occurence = 0;
+	for (int i = index; (c->sequence)[i] != '\0'; i++){
+		if ((c->sequence)[i] == direction){
+			count_occurence++;
+		}
+		else{
+			return count_occurence;
+		}
+	}
+	return count_occurence;
+}
+
+//--------------------------------------------------------------------------------
+
+
 void orientation(const char *sequence){
 /*E S W N */
 	
 }
+
+
+//--------------------------------------------------------------------------------
 
 int count_north(const char *sequence){
 	int north=0;
@@ -46,6 +98,8 @@ int count_north(const char *sequence){
 	return north;
 }
 
+//--------------------------------------------------------------------------------
+
 int count_west(const char *sequence){
 	int west=0;
 	for (int i=0; (sequence)[i] != '\0'; i++){
@@ -55,3 +109,7 @@ int count_west(const char *sequence){
 	}
 	return west;
 }
+
+//--------------------------------------------------------------------------------
+// end file
+//--------------------------------------------------------------------------------
